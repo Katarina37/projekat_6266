069f300da1e7a3df24edb8d96c639c253de4786c
katarina37 <katarinalazic37@gmail.com>
2025-01-30 12:06:18 +0100

    pocetak projekta

---
+++ Server/Klase/Igrac.cs
@@ -0,0 +1,47 @@
+﻿namespace Server.Klase
+{
+    public class Igrac
+    {
+        public int Id { get; set; }
+
+        public string Ime { get; set; }
+
+        public int[] PoeniPoIgrama { get; set; }
+
+        public bool Kvisko { get; set; }
+
+        public Igrac()
+        {
+        }
+
+        public Igrac(int id, string ime, int[] poeniPoIgrama, bool kvisko)
+        {
+            Id = id;
+            Ime = ime;
+            PoeniPoIgrama = poeniPoIgrama;
+            Kvisko = kvisko;
+        }
+
+        public void UloziKvisko()
+        {
+            Kvisko = true;
+        }
+
+        public void ResetujKvisko()
+        {
+            Kvisko = false;
+        }
+
+        public void DodajPoene(int indeksIgre, int poeni)
+        {
+            if (Kvisko)
+            {
+                PoeniPoIgrama[indeksIgre] += poeni * 2;
+            }
+            else
+            {
+                PoeniPoIgrama[indeksIgre] += poeni;
+            }
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Server
+    {
+        private const int UdpPort = 9000;
+        private const int TcpPort = 9001;
+        static void Main(string[] args)
+        {
+
+            Socket udpUticnica = new Socket(AddressFamily.InterNetwork, 
+                                            SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, UdpPort);
+            udpUticnica.Bind(endPoint);
+            Console.WriteLine($"Udp server slusa na portu {UdpPort}");
+
+            while (true)
+            {
+                byte[] prijemniBafer = new byte[1024];
+                EndPoint klijentEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                int brojPrimljenihBajtova = udpUticnica.ReceiveFrom(prijemniBafer, ref klijentEndPoint);
+
+                string poruka = Encoding.UTF8.GetString(prijemniBafer, 0, brojPrimljenihBajtova);
+                Console.WriteLine($"Primljena poruka za prijavu: {poruka} od {klijentEndPoint}");
+
+                if (poruka.StartsWith("PRIJAVA:"))
+                {
+                    ObradiPrijavuIgraca(poruka, klijentEndPoint, udpUticnica);
+                }       
+            
+            }
+        }
+
+        private static void ObradiPrijavuIgraca(string poruka, EndPoint klijentEndPoint, Socket udpUticnica)
+        {
+            string[] dijeloviPoruke = poruka.Substring(8).Split(',');
+            if(dijeloviPoruke.Length < 2)
+            {
+                Console.WriteLine("Neispravan format poruke za prijavu.");
+                return;
+            }
+
+            string imeIgraca = dijeloviPoruke[0].Trim();
+            string listaIgara = dijeloviPoruke[1].Trim();
+
+            string[] validneIgre = {"an", "po", "as"};
+            bool ispravnaListaIgara = listaIgara.Split(',').All(igra => validneIgre.Contains(igra.Trim()));
+
+            if (!ispravnaListaIgara)
+            {
+                Console.WriteLine("Neispravna lista igara.");
+                return;
+            }
+
+            Console.WriteLine($"Igrac {imeIgraca} zeli da igra igre: {listaIgara}");
+
+            string tcpInformacije = $"TCP: 127.0.0.1:{TcpPort}";
+            byte[] odgovorPodaci = Encoding.UTF8.GetBytes(tcpInformacije);
+            udpUticnica.SendTo(odgovorPodaci, klijentEndPoint);
+
+            PokreniTcpKomunikaciju(imeIgraca);
+        }
+
+        private static void PokreniTcpKomunikaciju(string imeIgraca)
+        {
+            Socket tcpUticnica = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            tcpUticnica.Bind(new IPEndPoint(IPAddress.Any, TcpPort));
+            tcpUticnica.Listen(10);
+
+            Console.WriteLine($"Tcp server slusa na portu {TcpPort}");
+
+            while (true)
+            {
+                Socket klijentSocket = tcpUticnica.Accept();
+                Console.WriteLine($"Uspostavljena TCP konekcija sa igracem.");
+
+                string porukaDobrodoslice = $"Dobrodosli u trening igru kviza Kviskoteke, danasnji tackmicar je {imeIgraca}";
+                byte[] dobrodosliPodaci = Encoding.UTF8.GetBytes(porukaDobrodoslice);
+                klijentSocket.Send(dobrodosliPodaci);
+
+                Console.WriteLine("Poruka dobrodoslice poslata.");
+                klijentSocket.Close();
+                break;
+            }
+            tcpUticnica.Close();
+        }
+    }
+}
