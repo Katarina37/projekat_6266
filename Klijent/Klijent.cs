cd817003199b1445f7ea2447458e237d7a042028
katarina37 <katarinalazic37@gmail.com>
2025-02-03 11:11:10 +0100

    izmjena klijenta

--- Klijent/Klijent.cs
+++ Klijent/Klijent.cs
@@ -16,41 +16,49 @@ namespace Klijent
         {
             try
             {
+                Console.WriteLine("Unesite ime/nadimak igraca: \n");
+                string imeIgraca = Console.ReadLine();
+
+                Console.WriteLine("\nUnesite igre koje zelite da igrate (an, po, as) odvojene zarezom: ");
+                string igre = Console.ReadLine();
+
+                string prijavaPoruka = $"PRIJAVA:{imeIgraca},{igre}";
+
                 //udp prijava
                 using (Socket udpKlijent = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
                 {
-                    string prijavaPoruka = "PRIJAVA:ImeIgraca,an,po";
                     byte[] podaci = Encoding.UTF8.GetBytes(prijavaPoruka);
                     udpKlijent.SendTo(podaci, new IPEndPoint(IPAddress.Parse(ServerIP), UdpPort));
-                    Console.WriteLine("Poslata UDP prijava.\n");
+                    Console.WriteLine("\nPoslata UDP prijava.\n");
 
-                    //cekanje odgovora
-                    byte[] prijemniiBafer = new byte[1024];
+                    byte[] prijemniBafer = new byte[1024];
                     EndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 0);
-                    int brPrimljenihBajtova = udpKlijent.ReceiveFrom(prijemniiBafer, ref serverEndPoint);
-                    string odg = Encoding.UTF8.GetString(prijemniiBafer, 0, brPrimljenihBajtova);
-                    Console.WriteLine($"Server odgovorio:{odg}.\n");
+                    int brPrimljenihBajtova = udpKlijent.ReceiveFrom(prijemniBafer, ref serverEndPoint);
+                    string odg = Encoding.UTF8.GetString(prijemniBafer, 0, brPrimljenihBajtova);
+                    Console.WriteLine($"Server odgovorio: {odg}\n");
                 }
 
-                //povezujemo se na server
-                Socket klijentSocet = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                klijentSocet.Connect(new IPEndPoint(IPAddress.Parse(ServerIP), TcpPort));
-                Console.WriteLine("Povezan sa serverom.\n");
+                using (Socket klijentSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    klijentSocket.Connect(new IPEndPoint(IPAddress.Parse(ServerIP), TcpPort));
+                    Console.WriteLine("Povezan sa serverom.\n");
 
-                //primamo poruku od servera
-                byte[] prijemniBafer = new byte[1024];
-                int brojPrimljenihBajtova = klijentSocet.Receive(prijemniBafer);
-                string poruka = Encoding.UTF8.GetString(prijemniBafer, 0, brojPrimljenihBajtova);
-                Console.WriteLine($"Server: {poruka}.\n");
+                    byte[] prijemniBafer = new byte[1024];
+                    int brojPrimljenihBajtova = klijentSocket.Receive(prijemniBafer);
+                    string poruka = Encoding.UTF8.GetString(prijemniBafer, 0, brojPrimljenihBajtova);
+                    Console.WriteLine($"Server : {poruka}\n");
 
-                //saljemo poruku start
-                string odgovor = "START";
-                byte[] startPodaci = Encoding.UTF8.GetBytes(odgovor);
-                klijentSocet.Send(startPodaci);
-                Console.WriteLine("Poslata poruka START.\n");
+                    Console.WriteLine("Unesite START za pocetak igre: ");
+                    string odgovor = Console.ReadLine();
+                    byte[] startPodaci = Encoding.UTF8.GetBytes(odgovor);
+                    klijentSocket.Send(startPodaci);
+                    Console.WriteLine("Poslata poruka START.\n");
 
-                klijentSocet.Shutdown(SocketShutdown.Both);
-                klijentSocet.Close();
+                    klijentSocket.Shutdown(SocketShutdown.Both);
+                    klijentSocket.Close();
+                }
+
+                
             }
             catch (Exception ex)
             {
